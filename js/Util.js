8a9a8f6ae1ec22be138aa627637a53a02f7aa3e1
LarsPaulsson <125218220+LarsPaulsson@users.noreply.github.com>
2024-12-04 22:49:57 +0100

    pulsr

--- js/Util.js
+++ js/Util.js
@@ -12,10 +12,10 @@ const MSG_LOADED=91;
 const MINPRICE=1;
 const MAXPRICE=10;
 
-let product1 = { number:1, name:"Vete", price:1.0, volume:1000, pulses: 0.22, image:"img/vete.jpg"};
-let product2 = { number:2, name:"Fågelfrö", price:5.5, volume:100, pulses: 0.44, image:"img/fågelfrö.jpg" };
-let product3 = { number:3, name:"Majs", price:3.8, volume:500, pulses: 0.32, image:"img/majs.jpg" };
-let product4 = { number:4, name:"Havre", price:3.9, volume:2000, pulses: 0.25, image:"img/havre.jpg" };
+let product1 = { number:1, name:"Vete", price:1.0, volume:1000, pulses: 4.48, image:"img/vete.jpg"};
+let product2 = { number:2, name:"Fågelfrö", price:5.5, volume:100, pulses: 6, image:"img/fågelfrö.jpg" };
+let product3 = { number:3, name:"Majs", price:3.8, volume:500, pulses: 5, image:"img/majs.jpg" };
+let product4 = { number:4, name:"Havre", price:3.9, volume:2000, pulses: 4.8, image:"img/havre.jpg" };
 let product5 = { number:0, name:"QRkoder", price:0, volume:20, pulses: 1, image:"img/qr.png" };
 let product6 = { number:0, name:"QRkoder", price:0, volume:20, pulses: 1, image:"img/qr.png" };
 // let prods=[product1,product2,product3,product4,product5,product6];
